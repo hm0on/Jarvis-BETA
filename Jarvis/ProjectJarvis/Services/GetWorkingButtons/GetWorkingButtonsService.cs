51823f677d1f62abbcdf8c26da584a5495521e53
rtyt20 <123096124+rtyt20@users.noreply.github.com>
2024-09-30 20:05:03 +0200

    add GetWorkingButtonsService.cs

---
+++ Jarvis/ProjectJarvis/Services/GetWorkingButtons/GetWorkingButtonsService.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Navigation;
+
+namespace Jarvis.ProjectJarvis.Services.GetWorkingButtons;
+
+public static class GetWorkingButtonsService
+{
+    public static Button GetWorkingButtons(NavigationService navigationService, Window windowToButton, string buttonName)
+    {
+        var window = navigationService.Content as Window;
+        var controlFrameChild = window!.FindName(buttonName) as Button;
+        
+        return controlFrameChild;
+    }
+}

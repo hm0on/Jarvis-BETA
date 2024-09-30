57b413d6540d203ad018d89ff5abcd43040dbfac
rtyt20 <123096124+rtyt20@users.noreply.github.com>
2024-09-30 20:50:28 +0200

    added ButtonList.cs

---
+++ Jarvis/ProjectJarvis/Helpers/ButtonList.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Windows.Documents;
+
+namespace Jarvis.ProjectJarvis.Helpers;
+
+public class ButtonListClass
+{
+    public List<string> ButtonList = ["MainGridButton", "InformationGridButton", "CommandListButton", 
+        "SettingsGridButton", "ProfileButton"];
+}
